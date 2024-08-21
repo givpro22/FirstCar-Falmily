2f33d547ce8a20c95c5027a01f57d5897014ef3c
박영서 <givpro22@daum.net>
2024-08-22 00:54:11 +0900

    00:54

--- build/해커톤 주제#3/app.py
+++ build/해커톤 주제#3/app.py
@@ -25,22 +25,19 @@ def index():
         df = process_data(files)  # 데이터 전처리 함수 호출 data.py에 있는 함수
         df = process_1data(df)  # 데이터 가공 함수 호출 sum.py에 있는 함수
         # 엑셀 파일을 메모리에 저장
+        
+        
+        
         output = BytesIO()
-        
-        
-        
         with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
             df.to_excel(writer, index=False)
         output.seek(0)  # 파일 포인터를 시작 위치로 이동
-
         # 생성된 엑셀 파일을 다운로드할 수 있게 반환
         return send_file(output, as_attachment=True, download_name='processed_data.xlsx', mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
     
+    
+    
     return render_template('index.html')
-
-@app.route('/FirstCar', methods=['GET'])
-def first_car():
-    return render_template('first_car.html')  # /FirstCar에 대한 템플릿 렌더링
 
 
 if __name__ == '__main__':
