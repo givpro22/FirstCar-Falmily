51fd620a1a97f66112e734c358b25c03f62ae34c
Yang <yjhyoyo52@gmail.com>
2024-08-22 10:44:38 +0900

    10:44

--- build/해커톤 주제#3/app.py
+++ build/해커톤 주제#3/app.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from io import BytesIO
-from fpdf import FPDF  # fpdf2 라이브러리에서 'fpdf' 모듈을 사용
+from fpdf import FPDF 
 from PyPDF2 import PdfMerger
 
 from data import process_data, process_2data, process_3data, process_cause, analyze
@@ -13,57 +13,9 @@ from xlsxwriter import *
 from mf_exp_sum import manufacturing_1data
 from raw_ma_sum import manufacturing_2data, manufacturing_3data
 from visual import visual
+from pdf_merge import merge_pdfs
+from report import generate_pdf, generate_pdf2
 
-#from report import generate_pdf
-def generate_pdf(dataframe):
-    pdf = FPDF()
-    pdf.add_page()
-    # 유니코드 폰트 추가
-    pdf.add_font('Nanum', '', "C:/Users/jeong/OneDrive/바탕 화면/NanumGothic/NanumGothic-Regular.ttf", uni=True)
-    pdf.set_font('Nanum', size=10)
-    
-    # 테이블 헤더 작성
-    col_widths = [50, 30, 30, 50]  # 열 너비 설정
-    pdf.cell(col_widths[0], 10, '계정명', border=1, align='C')
-    pdf.cell(col_widths[1], 10, '2022년', border=1, align='C')
-    pdf.cell(col_widths[2], 10, '2023년', border=1, align='C')
-    pdf.cell(col_widths[3], 10, '전기대비 증감율', border=1, align='C')
-    pdf.ln()
-
-    # 데이터프레임의 각 행을 PDF에 추가
-    for index, row in dataframe.iterrows():
-        pdf.cell(col_widths[0], 10, str(row['계정명']), border=1)
-        pdf.cell(col_widths[1], 10, f"{int(row['2022년']):,}", border=1, align='R')
-        pdf.cell(col_widths[2], 10, f"{int(row['2023년']):,}", border=1, align='R')
-        pdf.cell(col_widths[3], 10, str(row['전기대비 증감율']), border=1, align='R')
-        pdf.ln()
-
-    return pdf
-
-def generate_pdf2(dataframe):
-    pdf = FPDF()
-    pdf.add_page()
-    # 유니코드 폰트 추가
-    pdf.add_font('Nanum', '', "C:/Users/jeong/OneDrive/바탕 화면/NanumGothic/NanumGothic-Regular.ttf", uni=True)
-    pdf.set_font('Nanum', size=10)
-    
-    # 테이블 헤더 작성
-    col_widths = [50, 40, 50, 40]  # 열 너비 설정
-    pdf.cell(col_widths[0], 10, '원재료명', border=1, align='C')
-    pdf.cell(col_widths[1], 10, '2022 총 출고량', border=1, align='C')
-    pdf.cell(col_widths[2], 10, '원재료명', border=1, align='C')
-    pdf.cell(col_widths[3], 10, '2023 총 출고량', border=1, align='C')
-    pdf.ln()
-
-    # 데이터프레임의 각 행을 PDF에 추가
-    for index, row in dataframe.iterrows():
-        pdf.cell(col_widths[0], 10, str(row['원재료명']), border=1)
-        pdf.cell(col_widths[1], 10, str(row.get('2022 총 출고량', '')), border=1, align='R')
-        pdf.cell(col_widths[2], 10, str(row['원재료명']), border=1, align = "R")
-        pdf.cell(col_widths[3], 10, str(row.get('2023 총 출고량', '')), border=1, align='R')
-        pdf.ln()
-
-    return pdf
 
 
 #민혁 hi aaaaa
